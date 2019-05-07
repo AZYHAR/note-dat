198b33c0223c73bc32399fe2773bd9234e307eb8
Aleksey Glazkov <alexglazkov9703@gmail.com>
2019-05-07 17:58:18 -0400

    removed getAllUsers

--- client/src/_actions/user.actions.js
+++ client/src/_actions/user.actions.js
@@ -5,8 +5,7 @@ import { history } from '../_helpers';
 
 export const userActions = {
     login,
-    logout,
-    getAll
+    logout
 };
 
 function login(username, password) {
@@ -34,23 +33,4 @@ function login(username, password) {
 function logout() {
     userService.logout();
     return { type: userConstants.LOGOUT };
-}
-
-function getAll() {
-    return dispatch => {
-        dispatch(request());
-
-        userService.getAll()
-            .then(
-                users => dispatch(success(users)),
-                error => { 
-                    dispatch(failure(error));
-                    dispatch(alertActions.error(error))
-                }
-            );
-    };
-
-    function request() { return { type: userConstants.GETALL_REQUEST } }
-    function success(users) { return { type: userConstants.GETALL_SUCCESS, users } }
-    function failure(error) { return { type: userConstants.GETALL_FAILURE, error } }
 }
--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 
 import { authentication } from './authentication.reducer';
-import { users } from './users.reducer';
 import { alert } from './alert.reducer';
 
 const rootReducer = combineReducers({
   authentication,
-  users,
   alert
 });
 
