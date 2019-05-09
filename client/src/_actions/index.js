907689495ca66e0e22b51b0f539be3416aedcbad
Anton <tony@Tony.local>
2019-05-09 19:46:49 -0400

    Notes in client + created Actions for Notes and Notebooks

--- client/src/_actions/index.js
+++ client/src/_actions/index.js
@@ -1,2 +1,4 @@
 export * from './alert.actions';
 export * from './user.actions';
+export * from './note.action';
+export * from './notebook.action';
--- client/src/_constants/index.js
+++ client/src/_constants/index.js
@@ -1,2 +1,4 @@
 export * from './alert.constants';
 export * from './user.constants';
+export * from './notebook.constants';
+export * from './note.constants';
