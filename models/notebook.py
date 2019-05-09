4c92fed37d79e2bb966807b334eaadabd1815e65
Illia Hladush <illia.gladush@gmail.com>
2019-05-09 19:39:10 -0400

    relationship

--- models/notebook.py
+++ models/notebook.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from marshmallow import Schema, fields, pre_load, validate
 from models.db import db, ma
+from models.note import Note
 
 class Notebook(db.Model):
     __tablename__ = 'notebooks'
@@ -8,12 +9,15 @@ class Notebook(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text)
     creation_date = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'))
+    notes = db.relationship("Note", cascade="all,delete", backref = "notebooks")
     
     def __init__(self, title, creation_date):
         self.title = title
         self.creation_date = creation_date
 
+
 class NotebookSchema(ma.Schema):
-    id = fields.Integer(dump_only=True)
+    id = fields.Integer()
     title = fields.String(required=True)
     creation_date = fields.DateTime()
