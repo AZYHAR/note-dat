b0b5f0144506847760121b1d73ad5527d689c935
Aliaksandr Ushakou <ushakou.aleksandr@gmail.com>
2019-05-13 21:05:28 -0400

    renamed file

--- client/src/_actions/notebook.actions.js
+++ client/src/_actions/notebook.actions.js
@@ -1,28 +1,28 @@
 import { notebookConstants } from '../_constants/notebook.constants';
-import { notebookService } from '../_services';
+import { notebookService } from '../_services/notebook.service';
+import { alertActions } from './alert.actions';
 
-// We wrapped all Actions creators for easier accessing them in other files
 export const notebookActions = {
-    get_all
+    getAllNotebooks
 };
 
-// This is 1 Action Creator for alert Notebook Actions
-function get_all(){
+function getAllNotebooks() {
     return dispatch => {
-        dispatch(request());
+        dispatch(request({}));
 
         notebookService.getAll()
-        .then(
-            notebooks => dispatch(success(notebooks)),
-            error => { 
-                dispatch(failure(error));
-                dispatch(alertActions.error(error))
-            }
-        );
-
+            .then(
+                notebooks => {
+                    dispatch(success(notebooks.data));
+                },
+                error => {
+                    dispatch(failure(error));
+                    dispatch(alertActions.error(error));
+                }
+            );
     };
 
-    function request() { return { type: notebookConstants.GET_ALL_REQUEST} }
-    function success(notebooks) { return { type: notebookConstants.GET_ALL_SUCCESS, notebooks } }
-    function failure(error) { return { type: noteConstants.GET_ALL_FAILURE, error} }
+    function request(notebooks) { return { type: notebookConstants.NOTEBOOK_GETALL_REQUEST, notebooks } }
+    function success(notebooks) { return { type: notebookConstants.NOTEBOOK_GETALL_SUCCESS, notebooks } }
+    function failure(error) { return { type: notebookConstants.NOTEBOOK_GETALL_FAILURE, error } }
 }
