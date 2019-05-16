7b8d754d440ba2a7d11bf4139ae650dac8ea4ff4
Aleksey Glazkov <alexglazkov9703@gmail.com>
2019-05-15 20:24:26 -0400

    adds authentication token to axios header, when app loads

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,7 +4,13 @@ import { Provider } from 'react-redux';
 
 import { store } from './_helpers/store';
 import { App } from './App';
+import { setAuthHeader } from './_helpers';
 
+
+//On app load, checks if logged in and sets up Authentication header
+if(localStorage.getItem('user')){
+    setAuthHeader();
+}
 
 render(
     // Cover app Component with <Provider> to give access to the Redux Store
