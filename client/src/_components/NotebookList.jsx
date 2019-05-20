a3b06df246a1a1cda0745a08fda4deed6652ce05
Aliaksandr Ushakou <ushakou.aleksandr@gmail.com>
2019-05-19 22:13:48 -0400

    Updated Notebook list

--- client/src/_components/NotebookList.jsx
+++ client/src/_components/NotebookList.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 import { withStyles } from '@material-ui/core/styles';
 
 import { notebookActions } from '../_actions';
 
-const styles = ({
-    list: {
-      width: 250,
-      maxWidth: 250,
-      height: '100vw'
+const styles = theme => ({
+    container: {
+        width: '20%',
+        margin: theme.spacing.unit,
+        alignItems: 'center',
+        padding: 0,
     },
-    notebookTitle: {
-        fontize: 30
-    }
-  });
+    paperContainer: {
+        height: '100%',
+        padding: theme.spacing.unit,
+    },
+    listItem: {
+        fontSize: '1em',
+        marginTop: theme.spacing.unit,
+        boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)',
+    },
+    button: {
+        width: '100%',
+    },
+    leftIcon: {
+        marginRight: theme.spacing.unit,
+    },
+});
 
 class NotebookList extends React.Component {
     constructor(props) {
@@ -31,17 +46,23 @@ class NotebookList extends React.Component {
         const notebookList = [];
         notebooks.forEach((notebook) => {
             notebookList.push(
-                            <ListItem key={notebook.id} button>
-                                <ListItemText classes={classes.notebokTitle} primary={notebook.title} />
-                            </ListItem>
-                        );
+                <ListItem key={notebook.id} button className={classes.listItem}>
+                    <ListItemText primary={notebook.title} />
+                </ListItem>
+            );
         });
 
         return (
-            <div className={classes.list}>
-                <List>
-                    {notebookList}
-                </List>
+            <div className={classes.container}>
+                <Paper className={classes.paperContainer}>
+                    <Button variant="contained" color="default" className={classes.button}>
+                        <AddIcon className={classes.leftIcon} />
+                        Create Notebook
+                    </Button>
+                    <List className={classes.list}>
+                        {notebookList}
+                    </List>
+                </Paper>
             </div>
         )
     }
