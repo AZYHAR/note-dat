1c2460af55971efa372357430faca9e64b236146
Aleksey Glazkov <alexglazkov9703@gmail.com>
2019-05-20 19:16:25 -0400

    applied styling from Login component to SignUp component

--- client/src/SignUpPage/SignUpPage.jsx
+++ client/src/SignUpPage/SignUpPage.jsx
@@ -2,6 +2,44 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { userActions } from '../_actions';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    container: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+    footer: {
+        marginTop: theme.spacing.unit * 3,
+    },
+});
 
 class SignUpPage extends React.Component {
     constructor(props) {
@@ -39,50 +77,47 @@ class SignUpPage extends React.Component {
     }
 
     render() {
-        const { signingUp } = this.props;
+        const { signingUp, classes } = this.props;
         const { username, password, name, password_confirm, submitted } = this.state;
         return (
-            <div className="col-md-6 col-md-offset-3">
-                <h2>Sign Up</h2>
-                <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !name ? ' has-error' : '')}>
-                        <label htmlFor="username">Name</label>
-                        <input type="text" className="form-control" name="name" value={name} onChange={this.handleChange} />
-                        {submitted && !name &&
-                            <div className="help-block">Name is required</div>
-                        }
+            <div className={classes.container}>
+                <Paper className={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <form className={classes.form} onSubmit={this.handleSubmit}>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="name">Name</InputLabel>
+                            <Input id="name" name="name" value={name} onChange={this.handleChange} autoFocus />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="username">Username</InputLabel>
+                            <Input id="username" name="username" value={username} onChange={this.handleChange} autoFocus />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" name="password" type="password" value={password} onChange={this.handleChange} />
+                        </FormControl>
+                        <FormControl margin="normal" required fullWidth>
+                            <InputLabel htmlFor="password_confirm">Password</InputLabel>
+                            <Input id="password_confirm" name="password_confirm" type="password" value={password_confirm} onChange={this.handleChange} />
+                        </FormControl>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign up
+                        </Button>
+                    </form>
+                    <div className={classes.footer}>
+                        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                            Already have an account? <Link to="/login">Login here!</Link>
+                        </Typography>
                     </div>
-                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-                        <label htmlFor="username">Email</label>
-                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-                        {submitted && !username &&
-                            <div className="help-block">Email is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-                        {submitted && !password &&
-                            <div className="help-block">Password is required</div>
-                        }
-                    </div>
-                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-                        <label htmlFor="password">Confirm password</label>
-                        <input type="password" className="form-control" name="password_confirm" value={password_confirm} onChange={this.handleChange} />
-                        {submitted && !password_confirm &&
-                            <div className="help-block">Password confirmation is required</div>
-                        }
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary">Login</button>
-                        {signingUp &&
-                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                        }
-                    </div>
-                </form>
-                <p>
-                    <Link to="/login">Already have an account? Login here</Link>
-                </p>
+                </Paper>
             </div>
         );
     }
@@ -97,5 +132,5 @@ function mapStateToProps(state) {
     };
 }
 
-const connectedSignUpPage = connect(mapStateToProps)(SignUpPage);
+const connectedSignUpPage = withStyles(styles)(connect(mapStateToProps)(SignUpPage));
 export { connectedSignUpPage as SignUpPage }; 
