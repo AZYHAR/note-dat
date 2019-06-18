cbed4b1bf2e8a24056ab189d84a788c754fe9e7d
Anton Zyhar <azyhar@Antons-Mac-mini.local>
2019-06-18 19:50:45 -0400

    Created User Menu Navbar

--- client/src/_components/NavBar.jsx
+++ client/src/_components/NavBar.jsx
@@ -9,6 +9,12 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Link } from 'react-router-dom';
 
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const ITEM_HEIGHT = 48;
+
 const styles = {
   grow: {
     flexGrow: 1,
@@ -23,6 +29,56 @@ const styles = {
   }
 };
 
+const options = [
+  <Button><Link to="" className={styles.logoutButton}>Change Password</Link></Button>,
+  <Button><Link to="/login" className={styles.logoutButton}>Logout</Link></Button>,
+];
+
+function LongMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  function handleClick(event) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  return (
+    <div>
+      <IconButton
+        aria-label="More"
+        aria-controls="long-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <AccountCircle/>
+      </IconButton>
+      <Menu
+        id="long-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: 200,
+          },
+        }}
+      >
+        {options.map(option => (
+          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
+
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
@@ -30,20 +86,21 @@ class NavBar extends React.Component {
 
     render() {
         const { classes, user } = this.props;
-
+        
         return (
             <AppBar position="static">
               <Toolbar>
-                <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                  <MenuIcon />
-                </IconButton>
+                {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu"> */}
+                  {/* <MenuIcon />
+                </IconButton> */}
                 <Typography variant="h6" color="inherit" className={classes.grow}>
                   NoteDat
                 </Typography>
                 <Typography variant="h6" color="inherit" className={classes.grow}>
                   Hi {user.name}!
                 </Typography>
-                <Button><Link to="/login" className={classes.logoutButton}>Logout</Link></Button>
+                {/* <Button><Link to="/login" className={classes.logoutButton}>Logout</Link></Button> */}
+                <LongMenu/>
               </Toolbar>
             </AppBar>
         )
