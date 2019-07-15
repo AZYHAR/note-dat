61abba2b2cb76e291b310cfd47e2499b445738d4
Anton Zyhar <azyhar@Antons-MacBook-Pro.local>
2019-07-15 15:47:15 -0400

    make it better

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #parameters for SQL DATABASE
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+SQLALCHEMY_DATABASE_URI = "postgres://emdgasxpuybpur:e4c979ebe7fbb392ef8ea16f5d314e6b82e37c70ef88a2efd65a682fa1ad00a3@ec2-50-16-197-244.compute-1.amazonaws.com:5432/depie0b194vn8i"
 
 #token secret key
 JWT_SECRET_KEY = 'jwt-secret-key'
